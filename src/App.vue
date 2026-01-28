cf9a9d0dd4821b5e85dac2c2a677f03be22553cb
Abdellah Chadidi <chadidi97@gmail.com>
2026-01-28 19:08:19 +0100

    style: Remove default font-family from body CSS.

--- src/App.vue
+++ src/App.vue
@@ -243,7 +243,6 @@ html {
 body {
   margin: 0;
   padding: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
 }
 
 #hero {
